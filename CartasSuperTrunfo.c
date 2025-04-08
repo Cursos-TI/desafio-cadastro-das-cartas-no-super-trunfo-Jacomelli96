343b60e69165db8196feb4783e7bca58aebc2ee4
Jacomelli96 <jacomelli.mv@icloud.com>
2025-04-08 00:33:21 +0000

    super trunfo ok

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,25 +2,17 @@
 
 int main() {
     char EstadoA[20] = "Amazonas";
-    char EstadoB[20] = "Bahia";
-    char CodigoA[20] = "A01";
-    char CodigoB[20] = "A02";
-    char CidadeA[20] = "Manaus";
-    char CidadeB[20] = "Salvador";
-    int PopulaçãoA = 2064000;
-    int PopulaçãoB = 2418000;
-    float ÁreaA = 11.401;
-    float ÁreaB = 693.8;
-    float PibA = 103.281;
-    float PibB = 62.904;
-    int PontosturisticoA = 25;
-    int PontosturisticoB = 63;
-    int Carta = 1;
-    int Carta = 2;
+    char Carta[20] = "A01";
+    char Cidade [20] = "Manaus";
+    int População = 2000000;
+    float Área = 20000000;
+    int Turismo = 35;
 
-    printf("Desafio Xadrez!\n");
-   
-    printf("Digite a sua Carta: \n");
-    printf("Novo Commit\n");
+    printf("carta: %s\n", Carta);
 
+
+
+
+
+    return 0;
 }
